0ae68e673718be8f2aaa177b1dee785da2c26564
Luke Morales <lukemorales@live.com>
2019-08-06 23:09:36 -0300

    feat: Like and Dislike Controllers

---
+++ backend/src/controllers/DislikeController.js
@@ -0,0 +1,21 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+  async store(req, res) {
+    const { devID } = req.body.params;
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devID);
+
+    if (!targetDev) {
+      return res.status(400).json({error: 'Dev not found'});
+    };
+    
+    loggedDev.dislikes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev)
+  }
+}
---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,25 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+  async store(req, res) {
+    const { devID } = req.body.params;
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devID);
+
+    if (!targetDev) {
+      return res.status(400).json({error: 'Dev not found'});
+    };
+
+    if (targetDev.likes.includes(loggedDev._id)){
+      console.log(`Deu Match \o/`);
+    }
+
+    loggedDev.likes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev)
+  }
+}
