cc1029141db09c836e7b41b255fa6f96efa34ba0
Luke Morales <lukemorales@live.com>
2019-08-07 22:06:45 -0300

    chore: Main Page

---
+++ frontend/src/pages/Main/Main.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+// import {} from './Main_Styles';
+
+export default function Main() {
+  const [hook, setHook] = useState(hook);
+
+  return (
+    <>
+      <h1>Main Component</h1>
+    </>
+  );
+};
+
+
+
