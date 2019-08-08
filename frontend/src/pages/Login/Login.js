a32774c17cd0e1a4292946a6d9bcfc4c59d7ae23
Luke Morales <lukemorales@live.com>
2019-08-08 00:11:03 -0300

    feat: Functional Login Page

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from './Login_Styles';
 
 import logo from '~/assets/logo.svg';
+import api from '~/services/api';
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post('devs', {
+      username,
+    });
+
+    const { _id: ID } = response.data;
+
+    history.push(`/dev/${ID}`);
+  }
+
   return (
     <Container>
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="tinDev" />
-        <input placeholder="Type your GitHub username" />
+        <input placeholder="Type your GitHub username" value={username} onChange={e => setUsername(e.target.value)} />
         <button type="submit">Login</button>
       </form>
     </Container>
