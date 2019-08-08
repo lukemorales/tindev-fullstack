2a0ce3c558a70c1af0c928ccf9c303024ec8a4cf
Luke Morales <lukemorales@live.com>
2019-08-08 00:11:26 -0300

    feat: Functional Main Page

--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -1,15 +1,83 @@
 import React, { useState, useEffect } from 'react';
-// import {} from './Main_Styles';
+import { Link } from 'react-router-dom';
 
-export default function Main() {
-  const [hook, setHook] = useState(hook);
+import { Container } from './Main_Styles';
+
+import logo from '~/assets/logo.svg';
+import like from '~/assets/like.svg';
+import dislike from '~/assets/dislike.svg';
+
+import api from '~/services/api';
+
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('devs', {
+        headers: {
+          user: match.params.ID,
+        },
+      });
+
+      setUsers(response.data);
+    })();
+  }, [match.params.ID]);
+
+  async function handleDislike(ID) {
+    await api.post(`devs/${ID}/dislikes`, null, {
+      headers: {
+        user: match.params.ID,
+      },
+    });
+
+    setUsers(users.filter(user => user._id !== ID));
+  }
+
+  async function handleLike(ID) {
+    await api.post(`devs/${ID}/likes`, null, {
+      headers: {
+        user: match.params.ID,
+      },
+    });
+
+    setUsers(users.filter(user => user._id !== ID));
+  }
 
   return (
-    <>
-      <h1>Main Component</h1>
-    </>
+    <Container>
+      <header>
+        <Link to="/">
+          <img src={logo} alt="tinDev" />
+        </Link>
+      </header>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <header>
+                <img src={user.avatar} alt={user.name} />
+              </header>
+
+              <section>
+                <h2>{user.name}</h2>
+                <p>{user.bio}</p>
+              </section>
+
+              <footer>
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike Dev" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like Dev" />
+                </button>
+              </footer>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>Oops... there are no devs available at the moment. </div>
+      )}
+    </Container>
   );
-};
-
-
-
+}
--- frontend/src/pages/Main/Main_Styles.js
+++ frontend/src/pages/Main/Main_Styles.js
@@ -1,6 +1,89 @@
 import styled from 'styled-components';
+import { darken, lighten } from 'polished';
 
-export const $ = styled.`
-	
+export const Container = styled.main`
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 50px 0;
+  text-align: center;
+
+  header + div {
+    font-size: 20px;
+    color: #65666d;
+    font-weight: bold;
+    margin-top: 300px;
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 30px;
+    margin-top: 50px;
+
+    li {
+      display: flex;
+      flex-direction: column;
+
+      header {
+        max-width: 100%;
+        max-height: 300px;
+        border-radius: 4px 4px 0 0;
+        overflow: hidden;
+        background: #44475a;
+
+        img {
+          max-width: 100%;
+          object-fit: cover;
+        }
+      }
+
+      section {
+        flex: 1;
+        background: #44475a;
+        border: 1px solid #44475a;
+        padding: 15px 20px;
+        text-align: left;
+        border-radius: 0 0 4px 4px;
+
+        h2 {
+          font-size: 16px;
+          color: #fff;
+        }
+
+        p {
+          font-size: 14px;
+          line-height: 20px;
+          color: #ccccc7;
+          margin-top: 5px;
+        }
+      }
+
+      footer {
+        margin-top: 10px;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 10px;
+
+        button {
+          height: 50px;
+          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.5);
+          border-radius: 4px;
+          background: #44475a;
+          transition: all 150ms ease-in-out;
+
+          &:hover {
+            box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2);
+            transform: translateY(-1px);
+            background: ${lighten(0.03, '#44475a')};
+          }
+
+          &:active {
+            box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.8);
+            transform: translateY(1px);
+            background: ${darken(0.03, '#44475a')};
+          }
+        }
+      }
+    }
+  }
 `;
-
