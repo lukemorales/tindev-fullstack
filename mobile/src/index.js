84413eea97ab3cb8b4b856945234ae033a063e25
Luke Morales <lukemorales@live.com>
2019-08-09 02:01:56 -0300

    style: Transparent StatusBar on Android

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar translucent={true} backgroundColor={'transparent'} />
+      <Routes />
+    </>
+  );
 }
