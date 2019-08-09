0902804990b4713946526851dd02c26a220af3d1
Luke Morales <lukemorales@live.com>
2019-08-09 01:09:55 -0300

    Initial React Native Setup

---
+++ mobile/src/routes.js
@@ -0,0 +1,8 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Main from '~/pages/Main';
+import Login from '~/pages/Login';
+
+const Routes = createAppContainer(createSwitchNavigator({ Login, Main }));
+
+export default Routes;
