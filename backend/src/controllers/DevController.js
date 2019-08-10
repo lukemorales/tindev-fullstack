cfd1928979a951bbaa6b7973e629a92992888bd3
Luke Morales <lukemorales@live.com>
2019-08-10 00:31:09 -0300

    fix: Always Store Username in LowerCase and Trimming Spaces

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -21,7 +21,7 @@ module.exports = {
 
   async store(req, res) {
 
-    const { username } = req.body;
+    const username  = req.body.username.trim().toLowerCase();
 
     const userExists = await Dev.findOne({
       user: username
